cd1397bc782e5b299f623f6106f89e5a28e58b21
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-09-20 15:15:54 -0300

    Calculadora Simples somente soma

---
+++ calculadoraSimples/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculadoraSimples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, RoutedEventArgs e)
+        {
+            //Declarando as variáveis
+            double n1, n2, res;
+
+            //Efetuando a entrada de dados
+            n1 = Convert.ToDouble(txtN1.Text);
+            n2 = Convert.ToDouble(txtN2.Text);
+            //Efetuando o cálculo
+            res = n1 + n2;
+            //Exibindo o resultado convertido em string
+            lblResultado.Content = res.ToString();
+        }
+    }
+}
